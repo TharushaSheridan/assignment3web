bee2e6472b9df870d5ba21e646e90fe0c54efccd
Tharusha Liyanage <henegamt@sheridancollege.ca>
2024-03-21 22:45:52 -0400

    initial commit with creating files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { PetDetailsComponent } from './pet-details/pet-details.component';
+import { PetIndexComponent } from './pet-index/pet-index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    PetDetailsComponent,
+    PetIndexComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pet-index/pet-index.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-index',
+  templateUrl: './pet-index.component.html',
+  styleUrl: './pet-index.component.css'
+})
+export class PetIndexComponent {
+
+}
